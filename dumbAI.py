0f0078f738dd3ef1a205177dc6162c7c1a5a2ca3
DB Lee <dl654@cornell.edu>
2018-03-28 15:11:36 -0400

    dumbAI python transcription!

---
+++ dumbAI.py
@@ -0,0 +1,23 @@
+import Player
+import numpy as np
+
+class dumbAI(Player.iPlayer):
+    def __init__(self,name,money):
+        super().__init__(money,name)
+
+    def initial_bet(self):
+        # Bets 90 % of whatever he has
+        return 0.9*self.money
+
+    def to_hit(self, game_state, player_hand):
+        toHitOrNotToHit = np.random.rand(1)
+        if(toHitOrNotToHit < 0.5):
+            return True
+        else:
+            return False
+
+    def print_game_state(self,game_state):
+        pass
+        
+    def get_state(self):
+        pass
