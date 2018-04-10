a00ff45ced0c098a14bdf6cf5a5ff823405c8ae0
mf908 <mitchelfung@yahoo.com>
2018-04-09 22:35:13 -0400

    stuff

--- game.py
+++ game.py
@@ -24,20 +24,60 @@ def score_hand(hand):
 		score = 23
 	return score
 
+#number of games
+#average hand size
+#average bet
+#average score
+#bustrate
+#winrate
+class Profile():
+	def __init__(self):
+		self.num_games = 0
+		self.total_cards = 0
+		self.total_bets = 0
+		self.total_score = 0
+		self.busts = 0
+		self.wins = 0
+
+	def add_game(self, hand_size, bet, score, bust, won):
+		self.num_games += 1
+		self.total_cards += hand_size
+		self.total_bets += bet
+		self.total_score += score
+		self.busts += bust
+		self.wins += won
+
+	def get_stats(self):
+		stats = -np.ones(6)
+		stats[0] = self.num_games
+		if self.num_games == 0:
+			return stats
+		else:
+			stats[1] = self.total_cards/self.num_games
+			stats[2] = self.total_bets/self.num_games
+			if not (self.num_games == self.busts):
+				stats[3] = self.total_score/(self.num_games - self.busts)
+			stats[4] = self.busts/self.num_games
+			stats[5] = self.wins/self.num_games
+		return stats
+
 class BlackjackGame():
 
 	def __init__(self, player_list, dealer):
 		self.deck = np.ones(52)
 		self.players = player_list
 		self.dealer = dealer
+		self.dealer_profit = 0
 		self.visible = {}
 		hands = {}
 		bets = {}
+		profiles = {}
 		for player in player_list:
 			hands[player.get_id()] = np.zeros(13)
-			bets[player.get_id()] = player.initial_bet()
+			profiles[player.get_id()] = Profile()
 		hands[dealer.get_id()] = np.zeros(13)
 		self.hands = hands
+		self.profiles = profiles
 
 	def check_wins(self):
 		dealer_score = score_hand(self.hands[self.dealer.get_id()])
@@ -45,7 +85,7 @@ class BlackjackGame():
 		for i, player in enumerate(self.players):
 			score = score_hand(self.hands[player.get_id()])
 			
-			if(score == dealer_score):
+			if(score < dealer_score):
 				wins[i] = -1
 			elif(score > dealer_score):
 				wins[i] = 1
@@ -73,6 +113,23 @@ class BlackjackGame():
 				self.deal(player)
 		while(np.sum(self.hands[self.dealer.get_id()]) < 5 and self.dealer.to_hit(self.get_state(), self.hands[player.get_id()])):
 			self.deal(self.dealer)
+		wins = self.check_wins()
+		#update player profiles
+		for i, player in enumerate(self.players):
+			player_profile = self.profiles[player.get_id()]
+			hand = self.hands[player.get_id()]
+			score = score_hand(hand)
+			bust = False
+			if(score == -1):
+				score = 0
+				bust = True
+			player_profile.add_game(np.sum(hand), self.bets[player.get_id()], score, bust, wins[i])
+		self.get_profit(wins)
+
+	def clear_game(self):
+		for player in self.players:
+			self.bets[player.get_id()] = 0
+			self.hands[player.get_id()] = np.zeros(13)
 
 	def get_state(self):
 		state = np.empty(15 * len(self.players) + 14)
@@ -85,6 +142,18 @@ class BlackjackGame():
 		state[dealer_start:dealer_start+13] = self.visible[self.dealer.get_id()]
 		state[dealer_start+13] = np.sum(self.hands[self.dealer.get_id()]) - 1
 		return state
+
+	def get_profiles(self):
+		profiles = np.zeros(6 * len(self.players))
+		for i, player in enumerate(self.players):
+			player_start = 6 * i
+			profiles[player_start:player_start+6] = self.profiles[player.get_id()].get_stats()
+		return profiles
+
+	def get_profit(self, wins):
+		profit = 0
+		for i, player in enumerate(self.players):
+			self.dealer_profit += wins[i] * self.bets[player.get_id()]
 
 	def print_game(self):
 		print('Deck status')
